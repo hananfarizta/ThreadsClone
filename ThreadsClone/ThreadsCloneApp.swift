cbc41a4df46d7411e3ddece2da60abb98e4f5e4d
hananfarizta <hananfariztaa@gmail.com>
2024-08-13 16:21:13 +0700

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Hanan Farizta on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
